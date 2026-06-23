bcac4eca75dafa9ddf13a60e01240233ad9f845e
Yash Kumavat <yashnkumavat2005@gmail.com>
2026-06-24 00:45:09 +0530

    API URL updated

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "https://esprit-gridlock.onrender.com/api/v1";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://esprit-gridlock.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_URL,
